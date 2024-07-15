306c0e676459d3d1007beb27f235ca02075b842a
Colby Johnson <colbjohnson025@gmail.com>
2024-07-15 01:59:18 -0400

    msg

--- Project 3/src/main.cpp
+++ Project 3/src/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std; 
 
 int main() {
-    
-    cout << "Hello, World!" << endl;
+    cout << "Welcome to the Program" << endl;
+    Sleep(1000);
+    cout << "1. XXX" << endl;
+    cout << "2. XXX" << endl;
+    cout << "3. XXX" << endl;
+    cout << "4. XXX" << endl;
+    cout << "5. XXX" << endl;
+    cout << "6. XXX" << endl;
+    cout << "7. XXX" << endl;
+    cout << "8. XXX" << endl;
+    cout << "9. XXX" << endl;
+    cout << "e<X>it" << endl;
+    cout << "Select a region to analyze: ";
+
+    string resp;
+    cin >> resp; 
+
     return 0;
 }
